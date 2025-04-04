cdb902fdbcd2a6c294b1d9d2f06245a7bfaa59bc
takelele <work.mnikulin@gmail.com>
2025-04-04 22:05:52 +0500

    🔧 Параметры запуска локального сервера
    - Параметры локального сервера ( Хост и порт ) теперь зависят от файла settings
    - Добавлена дока OpenAPI
    - Стали передавать параметр DEBUG при инициализации приложения

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
 import uvicorn
 from litestar import Litestar
-from routes.users import router as users_router
+from litestar.openapi import OpenAPIConfig
 
-app = Litestar(route_handlers=[users_router])
+from routes.users import router as users_router
+from userhub import settings
+
+app = Litestar(
+    route_handlers=[users_router],
+    openapi_config=OpenAPIConfig(
+        title="UserHub API",
+        version="1.0.0",
+        use_handler_docstrings=True,
+    ),
+    debug=settings.DEBUG,
+)
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
+        host=settings.APPLICATION_HOST,
         port=8000
     )
